2e37c9083bdd2b66670beffa3929daf3a4dea7ee
fanz1 <fanz1@rose-hulman.edu>
2019-03-19 22:07:09 -0400

    Done

--- src/problem4.py
+++ src/problem4.py
@@ -3,8 +3,8 @@ Exam 1, problem 4.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Zixin Fan.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -80,10 +80,32 @@ def problem4(point1, point2, n, window):
       :type window:  rg.RoseWindow
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # IMPORTANT: For PARTIAL CREDIT, ignore the colors.
     # -------------------------------------------------------------------------
+    point1.attach_to(window)
+    point2.attach_to(window)
+
+    x = point1.x
+    y = point1.y
+
+    step_x = (point2.x - point1.x) / (2*n + 1)
+    step_y = (point2.y - point1.y) / (2*n + 1)
+
+    for k in range(2*n):
+        x = x + step_x
+        y = y + step_y
+
+        point = rg.Point(x, y)
+        point.attach_to(window)
+
+        if k <= (n-1):
+            point.fill_color = point2.fill_color
+        else:
+            point.fill_color = point1.fill_color
+
+    window.render()
 
 
 # -----------------------------------------------------------------------------
